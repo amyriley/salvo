57109bb1089c089226ef2a587ff941d3c0701dab
amrmynd <ariley.amy@gmail.com>
2019-11-13 18:40:51 +0100

    games table reformatting

--- build/resources/main/static/web/main.js
+++ build/resources/main/static/web/main.js
@@ -89,7 +89,6 @@ var app = new Vue({
             fetch(url, request)
                 .then(response => response.json())
                 .then(games => {
-                    console.log(games)
                     this.getGamePlayers;
                     this.games = games.games;
                     this.getPlayers(games.games);
@@ -753,11 +752,8 @@ var app = new Vue({
 
             for (var i = 0; i < games.length; i++) {
                 var game = games[i];
-                var createdDateTime = new Date(game.created).toLocaleString();
-                var players = this.getGamePlayers(game.gamePlayers);
-
-                gamesList.push({id: game.id, created: createdDateTime, 
-                    players: players, 
+                gamesList.push({id: game.id, created: game.created.toLocaleString(), 
+                    players: this.getGamePlayers(game), 
                     gamePlayerIds: this.getGamePlayerEmails(game)});
             }
 
@@ -766,10 +762,16 @@ var app = new Vue({
         gamePlayerIdsContains: function(n) {
             return this.gamePlayerIds.indexOf(n) > -1
         },
-        getGamePlayers: function(gamePlayers) {
+        getGamePlayers: function(game) {
+            var gamePlayers = [];
             var players = [];
             var gamePlayerIds = [];
             var emails = [];
+
+            for (var i = 0; i < game.gamePlayers.length; i++) {
+                var gamePlayer = game.gamePlayers[i];
+                gamePlayers.push(gamePlayer);
+            }
 
             for (var i = 0; i < gamePlayers.length; i++) {
                 var player = gamePlayers[i].player;
